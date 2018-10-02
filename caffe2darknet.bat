c4aa92725682000b151a607cf4339b33de20d7f5
vinjn <vinjn.z@gmail.com>
2018-10-02 21:49:08 +0800

    Update caffe2darknet.bat to support all the models

--- caffe2darknet.bat
+++ caffe2darknet.bat
@@ -1,1 +1,14 @@
-python ../lightnet/modules/pytorch-caffe-darknet-convert/caffe2darknet.py pose_deploy_linevec.prototxt pose_iter_440000.caffemodel pose_deploy.cfg pose_deploy.weights
+set OUTPUT=body_25
+set PROTO=../openpose/models/pose/body_25/pose_deploy.prototxt
+set MODEL=../openpose/models/pose/body_25/pose_iter_584000.caffemodel
+REM python ../lightnet/modules/pytorch-caffe-darknet-convert/caffe2darknet.py %PROTO% %MODEL% %OUTPUT%.cfg %OUTPUT%.weights
+
+set OUTPUT=coco
+set PROTO=../openpose/models/pose/coco/pose_deploy_linevec.prototxt
+set MODEL=../openpose/models/pose/coco/pose_iter_440000.caffemodel
+REM python ../lightnet/modules/pytorch-caffe-darknet-convert/caffe2darknet.py %PROTO% %MODEL% %OUTPUT%.cfg %OUTPUT%.weights
+
+set OUTPUT=mpi
+set PROTO=../openpose/models/pose/mpi/pose_deploy_linevec.prototxt
+set MODEL=../openpose/models/pose/mpi/pose_iter_160000.caffemodel
+python ../lightnet/modules/pytorch-caffe-darknet-convert/caffe2darknet.py %PROTO% %MODEL% %OUTPUT%.cfg %OUTPUT%.weights
